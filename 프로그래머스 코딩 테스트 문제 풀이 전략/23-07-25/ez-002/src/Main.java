30ca2da5ad073a51acfd3d1202dc84bf489f2162
EZ-000 <nongdam20@naver.com>
2023-07-24 11:00:47 +0900

    [doc] 은지 - 230725 모듈

---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-07-25/ez-002/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        P6 p6 = new P6();
+        String p6r1 = p6.solution1("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 25);
+        System.out.println(p6r1);
+    }
+}
