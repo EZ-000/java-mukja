d4f7feee212790b58ae7a18f128d9f7cd69f7a64
EZ-000 <nongdam20@naver.com>
2023-08-08 17:14:53 +0900

    [code] 은지 - 42840. 모의고사

--- 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-08/ez-004/src/P18.java
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-08/ez-004/src/P18.java
@@ -29,6 +29,7 @@ public class P18 {
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 
+    // 책 답안
     private static final int[][] RULES = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
