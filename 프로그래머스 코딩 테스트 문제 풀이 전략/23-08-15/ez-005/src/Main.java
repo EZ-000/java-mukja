f46d54c5645a3db895565e7f71c76d8d63ef0069
EZ-000 <nongdam20@naver.com>
2023-08-11 22:23:16 +0900

    [code] 은지 - 42839. 소수 찾기

--- 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-15/ez-005/src/Main.java
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-15/ez-005/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        P21 p21 = new P21();
+        p21.solution("17");
     }
 }
--- 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-15/ez-005/src/P21.java
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-15/ez-005/src/P21.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class P21 {
-    // 재귀는 상태, 종료조건, 점화식으로 구성
     private boolean isPrime(int n) {
         if (n <= 1) return false;
         for (int i = 2; i * i <= n; i++) {
@@ -10,12 +13,24 @@ public class P21 {
         return true;
     }
 
-    private void getPrimes(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes) {
-        if (isPrime(acc)) p
+    private Set<Integer> getPrimes(int acc, List<Integer> numbers) {
+        Set<Integer> primes = new HashSet<>();
+        if (isPrime(acc)) primes.add(acc);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int nextAcc = acc * 10 + numbers.get(i);
+            List<Integer> nextNumbers = new ArrayList<>(numbers);
+            nextNumbers.remove(i);
+            primes.addAll(getPrimes(nextAcc, nextNumbers));
+        }
+        return primes;
     }
 
     public int solution(String numbers) {
-        int answer = 0;
-        return answer;
+        List<Integer> nums = numbers.chars()
+                .map(c -> c - '0')
+                .boxed()
+                .collect(Collectors.toList());
+        return getPrimes(0, nums).size();
     }
 }
