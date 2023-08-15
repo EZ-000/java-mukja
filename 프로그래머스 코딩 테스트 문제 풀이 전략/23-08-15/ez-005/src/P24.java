ba10e604e2f9c9e6227aca1adff8a7baeffdc73e
EZ-000 <nongdam20@naver.com>
2023-08-15 16:16:39 +0900

    [code] 은지 - 68644. 두 개 뽑아서 더하기

---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-15/ez-005/src/P24.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class P24 {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().mapToInt(Integer::intValue).sorted().toArray();
+    }
+}
