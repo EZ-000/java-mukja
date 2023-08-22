8b4eb152edae96ad0f4988e4e05a93a82787f846
bbookng <bbookng@gmail.com>
2023-08-22 20:00:46 +0900

    [code] 보경 - 12915.문자열 내 마음대로 정렬하기

--- 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-22/bbookng/Solution27.java
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-08-22/bbookng/Solution27.java
@@ -18,7 +18,6 @@ public class 문자열_내_마음대로_정렬하기 {
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i).substring(1, list.get(i).length());
         }
-
         return answer;
     }
 
