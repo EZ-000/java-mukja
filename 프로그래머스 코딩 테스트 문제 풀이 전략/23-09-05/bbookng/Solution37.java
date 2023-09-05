c4843bc57245070f3e07666ba8d9ce16a4d047aa
bbookng <bbookng@gmail.com>
2023-09-05 20:41:30 +0900

    [code] 보경 - 42576. 완주하지 못한 선수

---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-09-05/bbookng/Solution37.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.*;
+
+public class Solution37 {
+    String answer = "";
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
