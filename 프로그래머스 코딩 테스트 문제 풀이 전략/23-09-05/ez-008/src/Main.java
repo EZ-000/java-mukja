aabc39fa2087dc1e527f7c94b78012d4d8a3b0c9
이은지 <nongdam20@naver.com>
2023-09-05 15:35:34 +0900

    [code] 은지 - 42576. 완주하지 못한 선수

--- 프로그래머스 코딩 테스트 문제 풀이 전략/23-09-05/ez-008/src/Main.java
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-09-05/ez-008/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        P37 p37 = new P37();
+        p37.solution2(new String[] {"leo", "kiki", "eden"}, new String[] {"eden", "kiki"});
     }
 }
