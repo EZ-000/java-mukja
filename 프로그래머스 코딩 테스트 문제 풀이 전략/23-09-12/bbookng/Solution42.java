6e0ccc2b2de2a8ae0190f6d94e4d8af113bc72e8
bbookng <bbookng@gmail.com>
2023-09-12 20:46:37 +0900

    [code] 보경 - 12909. 올바른 괄호

---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-09-12/bbookng/Solution42.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution42 {
+    boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
