475236d2371213b14061af1613087e3525962957
eunjineee <yej6642@naver.com>
2023-10-01 17:44:11 +0900

    [code] 은진 - 49190. 방의 개수

---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-09-26/eunjineee/P_48_49190.java
@@ -0,0 +1,62 @@
+package PGS_BOOK.CH11.CH11_02;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class P_48_49190 {
+    private static class Vertex {
+        public final int x;
+        public final int y;
+        public final String id;
+        public final Set<String> connectedVertices;
+
+        public Vertex(int x, int y){
+            this.x = x;
+            this.y = y;
+            this.id = id(x,y);
+            this.connectedVertices = new HashSet<>();
+        }
+        public static String id(int x, int y) {
+            return String.format("(%d, %d)", x, y);
+        }
+    }
+    private static final int[] dx = {0,1,1,1,0,-1,-1,-1};
+    private static final int[] dy = {-1,-1,0,1,1,1,0,-1};
+
+    public static int solution(int[] arrows){
+        int count = 0;
+
+        Map<String, Vertex> vertices = new HashMap<>();
+
+        Vertex v = new Vertex(0,0);
+        vertices.put(v.id, v);
+
+        for (int d : arrows) {
+            for (int i = 0; i < 2; i++) {
+                int x = v.x + dx[d];
+                int y = v.y + dy[d];
+                String id = Vertex.id(x, y);
+
+                if (!vertices.containsKey(id)) {
+                    vertices.put(id, new Vertex(x, y));
+                } else if (!v.connectedVertices.contains(id)) {
+                    count++;
+                }
+
+                Vertex u = vertices.get(id);
+                v.connectedVertices.add(u.id);
+                u.connectedVertices.add(v.id);
+                v = vertices.get(id);
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0};
+        System.out.println(solution(arr));
+    }
+}
