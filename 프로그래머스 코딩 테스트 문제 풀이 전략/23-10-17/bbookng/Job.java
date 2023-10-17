6a1058484286b0bcc993fbf298fdbf5d169b96d5
bbookng <bbookng@gmail.com>
2023-10-17 21:57:14 +0900

    [code] 보경 - 42627. 디스크 컨트롤러

---
+++ 프로그래머스 코딩 테스트 문제 풀이 전략/23-10-17/bbookng/Job.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class Job {
+    int start;
+    int duration;
+
+    public Job(int start, int duration) {
+        this.start = start;
+        this.duration = duration;
+    }
+}
